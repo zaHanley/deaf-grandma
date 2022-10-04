bd5e14915a8f1fe46d42454211cc3df23e5ffbe0
Zack Hanley <110628956+zaHanley@users.noreply.github.com>
2022-10-03 21:00:23 -0500

    Add files via upload

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,4 +1,3 @@
-// i wa
 function deafGrandma() {
     let input = prompt('HEY KID');
     let count = 0;
@@ -18,34 +17,8 @@ function deafGrandma() {
         } else {
             input = prompt('SPEAK UP, KID!')
         }
-    }
-    
-    
-
-    
-    
-        //         input = prompt('LEAVING SO SOON?');
-        //         count = 1;
-    // if (input === 'GOODBYE' && prompt('LEAVING SO SOON?') === 'GOODBYE') {
-    //     input = '';
-    //     return prompt('LATER SKATER!')
-    //     console.log(input)
-    // while (count = 0) {
-    //     
-    //     } else if (input.toUpperCase() === input && input != 'GOODBYE') {
-    //         input = prompt(`NO, NOT SINCE 1946!`);
-    //         count = 0;
-    //     } 
-        
-        
-    // } 
-    // else if (count === 1 && input.replace(/[^A-Za-z]/ig, '') === 'GOODBYE') {
-    //     return window.prompt('LATER SKATER')
-    // } console.log(input.replace(/[^A-Za-z]/ig) === input.toLowerCase()) 
-    //     input = prompt('SPEAK UP')
-    
-        
-    }
+    }        
+}
     
 
 
